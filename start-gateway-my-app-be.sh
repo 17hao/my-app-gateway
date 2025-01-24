27d866f573f34c76dcde6a956f919fe4fd3b2637
17hao <sqh1107@gmail.com>
2025-01-24 23:08:34 +0800

    fix ipv6

---
+++ start-gateway-my-app-be.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+export https_proxy=127.0.0.1:8889
+export http_proxy=127.0.0.1:8889
+
+container_name=gateway-my-app-be
+
+docker pull 17hao/my-app-gateway:latest
+
+docker stop ${container_name}
+
+docker rm ${container_name}
+
+docker rmi $(docker images -f "dangling=true" -q)
+
+docker run --name ${container_name} -d -p 10443:10443 17hao/my-app-gateway
